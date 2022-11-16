fafe2e7d0a6e025467ed98544b1ad46b9fdf2812
Alexis <alexis.mccarthy@vtsd.com>
2022-11-16 12:24:04 -0500

    lab 6

--- Lab06vst.java
+++ Lab06vst.java
@@ -24,10 +24,10 @@ public class Lab06vst extends Applet
         Random rand = new Random();
 
         for(k = 1; k <=100; k++){
-            int x1 = (rand.nextInt(390)+10);
-            int y1 = (rand.nextInt(290)+10);
-            int x2 = (rand.nextInt(390)+10);
-            int y2 = (rand.nextInt(290)+10);
+            int x1 = (rand.nextInt(391)+10);
+            int y1 = (rand.nextInt(291)+10);
+            int x2 = (rand.nextInt(391)+10);
+            int y2 = (rand.nextInt(291)+10);
             int c1 = (rand.nextInt(255));
             int c2 = (rand.nextInt(255));
             int c3 = (rand.nextInt(255));
@@ -41,8 +41,8 @@ public class Lab06vst extends Applet
         Random rand2 = new Random();
 
         for(k = 1; k <= 100; k++) {
-            int x1 = (rand2.nextInt(330) + 410);
-            int y2 = (rand2.nextInt(240) + 10);
+            int x1 = (rand2.nextInt(341) + 400);
+            int y2 = (rand2.nextInt(241) + 10);
             int c1 = (rand2.nextInt(255));
             int c2 = (rand2.nextInt(255));
             int c3 = (rand2.nextInt(255));
@@ -60,21 +60,37 @@ public class Lab06vst extends Applet
             int c2 = (rand1.nextInt(255));
             int c3 = (rand1.nextInt(255));
             int s1 = (rand1.nextInt(200));
-            int y2 = (rand1.nextInt(280)+300);
-            int x1 = (rand.nextInt(350)+10);
-            int rx1 = x1 - s1;
-            int ry2 = y2 - s1;
+            int y2 = (rand1.nextInt(291-s1)+300);
+            int x1 = (rand.nextInt(391-s1)+10);
+
 
 
             g.setColor(new Color(c1,c2,c3));
-            g.drawOval(rx1, ry2, s1, s1);
+            g.drawOval(x1, y2, s1, s1);
         }
 
         // Draw 3-D Box
+        g.setColor(new Color(255,255,0));
+        g.fillRect(550,350,100,100);
+
+        g.setColor(new Color(255,0,0));
+        g.fillRect(600,400,100,100);
+
+        g.setColor(new Color(0,255,0));
+        Polygon green = new Polygon();
+            green.addPoint(550,350);
+            green.addPoint(600,400);
+            green.addPoint(600,500);
+            green.addPoint(550,450);
+            g.fillPolygon(green);
 
 
-
-
+        g.setColor(new Color(0,0,255));
+        Polygon blue = new Polygon();
+            blue.addPoint(650,350);
+            blue.addPoint(650,400);
+            blue.addPoint(700,400);
+            g.fillPolygon(blue);
 
     }
 
